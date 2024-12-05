fa7e49a7eee3bd5af860e46fd5b97d0baabf5971
PatrickkikALT <45902985+PatrickkikALT@users.noreply.github.com>
2024-12-05 14:13:45 +0100

    Added program files
    Currently nothing more than printing

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <ostream>
+#include "ConsoleClass.h"
+int main(int argc, char* argv[])
+{
+    print("Test");
+    print("Amount test", 5);
+    system("pause");
+    return 0;
+}
+
---
+++ ConsoleApplication1/ConsoleClass.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+#include <iostream>
+
+class console_class {
+public:
+    static console_class& instance() {
+        static console_class instance;
+        return instance;
+    }
+
+    void print(const std::string& message) {
+        std::cout << message << '\n';
+    }
+
+    void print(const std::string& message, int amount) {
+        std::cout << message << ": " << amount << '\n';
+    }
+
+private:
+    console_class() = default; // Private constructor
+    console_class(const console_class&) = delete;
+    console_class& operator=(const console_class&) = delete;
+};
+inline void print(const std::string& message) {
+    console_class::instance().print(message);
+}
+
+inline void print(const std::string& message, int amount) {
+    for (int i = 0; i < amount ; i++) {
+        console_class::instance().print(message);
+    }
+}
